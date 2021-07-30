8387d89ae62a8bf9a13fef94b8270ddb6a427733
notinaboat <notinaboat@icloud.com>
2021-07-30 14:13:59 +1000

    add ARCH".." string for architecture docs, add index.md generation

--- src/ReadmeDocs.jl
+++ src/ReadmeDocs.jl
@@ -78,23 +78,136 @@ See also example [`Makefile`](Makefile).
 """
 module ReadmeDocs
 
-export @README_str
+export @README_str,
+       @ARCH_str
+
+
+const sections = ["README", "ARCH"]
+
+mutable struct ModuleDocs
+    mod::Module
+    docs::Vector{Tuple}
+    ModuleDocs(mod) = new(mod, [])
+end
+
+
+function search_meta(meta, path, line)
+    for (k, v) in meta
+        for doc in values(v.docs)
+            if doc.data[:linenumber] == line &&
+               doc.data[:path] == path
+                return doc.data
+           end
+       end
+    end
+end
+
+
+function generate(moddocs::ModuleDocs)
+
+    meta = moddocs.mod.eval(Docs.META)
+
+    if basename(pwd()) != string(moddocs.mod)
+        throw(ArgumentError("""
+            ReadmeDocs.generate should be run from the package home directory.
+            Expected `pwd()` to be \"$(moddocs.mod)\" (was \"$(pwd())\").
+            """))
+    end
+
+    output = Dict(x => [] for x in sections)
+    index = Dict(x => [] for x in sections)
+
+    push!(output["README"], string(Docs.doc(moddocs.mod)))
+
+    @info "Finding docstrings..."
+    for (type, doc, path, line) in moddocs.docs
+        x = search_meta(meta, path, line)
+        if x != nothing
+            name = string(x[:binding])
+            if doc[1] != "#"
+                doc = "\n### `$name`\n\n$doc"
+            end
+            push!(index[type], (:ref, name))
+        else
+            push!(index[type], (:text, doc))
+        end
+        push!(output[type], doc)
+    end
+    for (name, content) in output
+        @info "Generating $name.md"
+        write("$name.md", join(content, "\n"))
+    end
+
+    mkpath("docs/src/")
+
+    for (section, content) in index
+        if section == "README"
+            section = "index"
+            output = """
+                # $(moddocs.mod).jl
+
+                ```@docs
+                $(moddocs.mod)
+                ```
+                """
+        else
+            output = "# $section\n\n"
+        end
+        @info "Generating docs/src/$section.md"
+        for (type, x) in content
+            if type == :text
+                output *= "$x\n"
+            elseif type == :ref
+                output *= """
+                    ```@docs
+                    $x
+                    ```
+                    """
+            end
+        end
+        write("docs/src/$section.md", output)
+    end
+end
+
+
+const readme_docs_init = quote
+    global readme_docs
+    global readme_docs_generate
+    if !isdefined(@__MODULE__, :readme_docs)
+        const readme_docs = ReadmeDocs.ModuleDocs(@__MODULE__)
+        const readme_docs_generate = ()->ReadmeDocs.generate(readme_docs)
+    end
+end
 
 
 macro README_str(s)
+    s = Meta.parse("\"\"\"$s\"\"\"")
     esc(:(let s = $s
-          global readme_body
-          global readme
-          if !isdefined(@__MODULE__, :readme_body)
-              readme_body = ""
-              readme = () -> join([Docs.doc(@__MODULE__),
-                                   readme_body], "\n\n")
-          end
-          readme_body = string(readme_body, s, "\n\n")
+          $readme_docs_init
+          push!(readme_docs.docs,
+                ("README",
+                s,
+                $(string(__source__.file)),
+                $(Int(__source__.line))))
+          s
+      end))
+end
+
+macro ARCH_str(s)
+    s = Meta.parse("\"\"\"$s\"\"\"")
+    esc(:(let s = $s
+          $readme_docs_init
+          push!(readme_docs.docs,
+                ("ARCH",
+                s,
+                $(string(__source__.file)),
+                $(Int(__source__.line))))
           s
       end))
 end
 
 readme() = Docs.doc(@__MODULE__)
+
+
 
 end # module
